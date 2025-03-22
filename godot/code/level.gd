f9a728a3789933f4e95b6227ad029f09f390a869
Tiger Jove <tiger@tiger.blue>
2025-03-22 10:00:21 +0100

    Polish level 1 a little

--- godot/code/level.gd
+++ godot/code/level.gd
@@ -1,8 +1,5 @@
 extends Node3D
-class_name MyClass
-
-static func TheFunction() -> void:
-	pass
+class_name Level
 
 var player : Player
 var playing : bool
@@ -22,53 +19,9 @@ func _ready() -> void:
 
 	_main() # run in background
 
-static func global_something() -> void:
-	pass
 
-
-# Main event loop, currently dummy condition and processes input indefinitely
-func _main() -> void:
-	player = %Blox
-	Camera.follow = player
-	#Camera.look = player
-
-	#await %Intro1.play()
-
-	%Step1.show()
-
-	_play()
-	await player.moved
-
-	bind(player.moved, "moved")
-	bind(player.faces, "faces")
-
-	await any(["moved", "faces"])
-
-
-	%Step1.hide()
-	%Arrow0.hide()
-	%Arrow1.show()
-	%Step2.show()
-	await until(player.faces, %Mailbox)
-	%Mailbox.highlight.play()
-	%Arrow1.hide()
-	%Arrow2.show()
-	%Step2.hide()
-
-	%Waff1.show_for_critter(%Blox)
-	%Waff2.show_for_critter(%Blox)
-	%Waff3.show_for_critter(%Blox)
-
-	await until(player.faces, %Chip)
-	%Step3.show()
-	playing = await until(player.faces, %Blitty)
-	%Arrow2.hide()
-
-	await _cutscene()
-
-
-func _cutscene():
-	%Outro.show()
+func _main():
+	printerr("Level has no _main() function.")
 
 
 func until(awaitable: Signal, whom: Node) -> bool:
---
+++ godot/code/level_1.gd
@@ -0,0 +1,45 @@
+extends Level
+
+
+# Main event loop, currently dummy condition and processes input indefinitely
+func _main() -> void:
+	player = %Blox
+	Camera.follow = player
+	#Camera.look = player
+
+	#await %Intro1.play()
+
+	%Step1.show()
+
+	_play()
+	await player.moved
+
+	bind(player.moved, "moved")
+	bind(player.faces, "faces")
+
+	await any(["moved", "faces"])
+
+
+	%Step1.hide()
+	%Arrow0.hide()
+	%Arrow1.show()
+	%Step2.show()
+	await until(player.faces, %Mailbox)
+	%Mailbox.highlight.play()
+	%Arrow1.hide()
+	%Arrow2.show()
+	%Step2.hide()
+
+	%Waff1.show_for_critter(%Blox)
+	%Waff2.show_for_critter(%Blox)
+
+	await until(player.faces, %Chip)
+	%Step3.show()
+	playing = await until(player.faces, %Blitty)
+	%Arrow2.hide()
+
+	await _cutscene()
+
+
+func _cutscene():
+	%Outro.show()
